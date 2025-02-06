f5b8d247bbd03d216d7a242166ec4d745889f5a2
Void <voidsemitori@gmail.com>
2025-02-06 17:08:22 +0300

    Объявления класса "Chat", его основных переменных и методов.

---
+++ ConsoleChat/Chat.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include "User.h"
+
+class Chat {
+private:
+	std::vector <User> _users; //Контейнер, хранящий зарегистрированных пользователей
+	std::vector <std::string> _chat; //Контейнер, хранящий сообщения общего чата
+	User* currentUser = nullptr;  // Указатель на текущего пользователя
+	User* findByLogin(std::string& login); //Функция поиска пользователя по логину
+
+public:
+	void registerUser();
+	void login();
+	void sendPrivateMessage();
+	void sendMessageToChat();
+	void viewPrivateMessages();
+	void viewChat();
+	void logout();
+};
--- ConsoleChat/User.h
+++ ConsoleChat/User.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <string>
-
+#include <vector>
 
 class User {
 private:
 	std::string _name;
 	std::string _login;
 	std::string _password;
+	std::vector <std::pair<std::string, std::string>> _privateMessages;
 public:
 	User() = default; 
 	User(std::string& name, std::string& login, std::string& password); //Конструктор класса
