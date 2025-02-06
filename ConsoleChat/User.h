0144a69c961ccfeaf8311908e022ce1da40988ef
Void <voidsemitori@gmail.com>
2025-02-06 15:23:42 +0300

    Объявлние класса "User", его основных переменных и конструкторов

--- ConsoleChat/User.h
+++ ConsoleChat/User.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include <string>
+
+
+class User {
+private:
+	std::string _name;
+	std::string _login;
+	std::string _password;
+public:
+	User() = default; 
+	User(std::string& name, std::string& login, std::string& password); //Конструктор класса
+};
